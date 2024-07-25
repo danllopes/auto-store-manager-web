f901da46889fa4123cba2efe964ffe823b2fb59e
“Daniel <danielanderson.lopes0@gmail.com>
2024-07-25 14:18:23 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/danllopes/autostoreweb/AutoStoreWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.danllopes.autostoreweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoStoreWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoStoreWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danllopes/autostoreweb/AutoStoreWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danllopes.autostoreweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoStoreWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
