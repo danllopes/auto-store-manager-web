90396d9ec27c65adfc3fc04ac5d7de50c6b66aa6
“Daniel <danielanderson.lopes0@gmail.com>
2024-07-25 20:02:13 -0300

    ✨ feat: adiciona enum relacionado a classe Motorcycle

---
+++ src/main/java/br/com/danllopes/autostoreweb/domain/enums/BikeType.java
@@ -0,0 +1,13 @@
+package br.com.danllopes.autostoreweb.domain.enums;
+
+public enum BikeType {
+
+    ADVENTURE,
+    CHOPPER,
+    CRUISER,
+    NAKED,
+    SCOOTER,
+    SPORT,
+    STANDARD,
+    TOURING
+}
