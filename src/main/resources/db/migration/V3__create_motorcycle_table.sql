798e04219c21ab69533126818b51624c3c2212c7
“Daniel <danielanderson.lopes0@gmail.com>
2024-08-01 16:13:23 -0300

    💾 fix: adiciona chave estrangeira na tabela 'motorcycle'

--- src/main/resources/db/migration/V3__create_motorcycle_table.sql
+++ src/main/resources/db/migration/V3__create_motorcycle_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `motorcycle` (
-    `id` VARCHAR(36) PRIMARY KEY UNIQUE NOT NULL,
+    `id` VARCHAR(36) PRIMARY KEY NOT NULL,
     `cylinder_capacity` SMALLINT NOT NULL,
-    `bike_type` VARCHAR(10) NOT NULL
+    `bike_type` VARCHAR(10) NOT NULL,
+    FOREIGN KEY (`id`) REFERENCES `vehicle`(`id`)
 )
