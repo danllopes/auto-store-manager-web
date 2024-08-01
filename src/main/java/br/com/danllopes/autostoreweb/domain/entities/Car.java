c527e796562ec7bb2260dd8fa6633f459aa2c0b1
“Daniel <danielanderson.lopes0@gmail.com>
2024-08-01 15:59:32 -0300

    ✨ feat: Adiciona classe Car que estende classe Vehicle

---
+++ src/main/java/br/com/danllopes/autostoreweb/domain/entities/Car.java
@@ -0,0 +1,52 @@
+package br.com.danllopes.autostoreweb.domain.entities;
+
+import br.com.danllopes.autostoreweb.domain.enums.CarType;
+import br.com.danllopes.autostoreweb.dtos.CarRequestDTO;
+import jakarta.persistence.*;
+
+@Entity(name = "car")
+@Table(name = "car")
+public class Car extends Vehicle{
+
+    @Column(name = "num_doors")
+    private Byte numberOfDoors;
+
+    @Column(name = "engine_capacity")
+    private Double engineCapacity;
+
+    @Column(name = "car_type")
+    private CarType carType;
+
+    public Car() {super();}
+
+    public Car(CarRequestDTO data) {
+        super(data.brand(), data.model(), data.modelYear(), data.condition(), data.fuelType(), data.transmissionType(), data.mileage());
+        this.numberOfDoors = data.numberOfDoors();
+        this.engineCapacity = data.engineCapacity();
+        this.carType = CarType.valueOf(data.carType());
+    }
+
+    public Byte getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(Byte numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public Double getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(Double engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public void setCarType(CarType carType) {
+        this.carType = carType;
+    }
+}
