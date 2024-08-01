6bbc8e63c10504bbfddd9237c8b1dde19b153455
“Daniel <danielanderson.lopes0@gmail.com>
2024-08-01 16:19:09 -0300

    ✨ feat: Adiciona interface MotorcycleRepository

---
+++ src/main/java/br/com/danllopes/autostoreweb/repositories/MotorcycleRepository.java
@@ -0,0 +1,9 @@
+package br.com.danllopes.autostoreweb.repositories;
+
+import br.com.danllopes.autostoreweb.domain.entities.Motorcycle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MotorcycleRepository extends JpaRepository<Motorcycle, String> {
+}
