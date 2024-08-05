df5cc4e2ec9855c350ef224ac759919317e8f02d
“Daniel <danielanderson.lopes0@gmail.com>
2024-08-05 11:40:19 -0300

    ✨ feat: Adiciona métodos para atualizar e deletar(mudar o status para SOLD) o objeto motorcycle

--- src/main/java/br/com/danllopes/autostoreweb/services/MotorcycleService.java
+++ src/main/java/br/com/danllopes/autostoreweb/services/MotorcycleService.java
@@ -1,6 +1,7 @@
 package br.com.danllopes.autostoreweb.services;
 
 import br.com.danllopes.autostoreweb.domain.entities.Motorcycle;
+import br.com.danllopes.autostoreweb.domain.enums.VehicleStatus;
 import br.com.danllopes.autostoreweb.domain.exceptions.MotorcycleNotFoundException;
 import br.com.danllopes.autostoreweb.dtos.MotorcycleRequestDTO;
 import br.com.danllopes.autostoreweb.repositories.MotorcycleRepository;
@@ -17,13 +18,33 @@ public class MotorcycleService {
         this.repository = repository;
     }
 
-    public List<Motorcycle> getAllMotorcycles() {return this.repository.findAll();}
+    public List<Motorcycle> getAllMotorcycles() {return this.repository.findByStatus(VehicleStatus.FOR_SALE);}
 
     public Motorcycle getOptionalMotorcycle(String id) {return this.findMotorcycleById(id);}
 
     public Motorcycle createMotorcycle(MotorcycleRequestDTO data) {
         var motorcycle = new Motorcycle(data);
         return this.repository.save(motorcycle);
+    }
+
+    public Motorcycle updateMotorcycle(MotorcycleRequestDTO data, String id) {
+        var optionalBike = this.findMotorcycleById(id);
+        optionalBike.setBrand(data.brand());
+        optionalBike.setModel(data.model());
+        optionalBike.setModelYear(data.modelYear());
+        optionalBike.setCondition(data.condition());
+        optionalBike.setFuelType(data.fuelType());
+        optionalBike.setTransmissionType(data.transmissionType());
+        optionalBike.setMileage(data.mileage());
+        optionalBike.setCylinderCapacity(data.cylinderCapacity());
+        optionalBike.setBikeType(data.bikeType());
+        this.repository.save(optionalBike);
+        return optionalBike;
+    }
+
+    public void updateToSold(String id) {
+        var optionalBike = this.findMotorcycleById(id);
+        optionalBike.updateToSold();
     }
 
     private Motorcycle findMotorcycleById(String id) {
