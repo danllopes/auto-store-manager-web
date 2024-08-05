ee9789e2fdf6b9fec83b82532dea0374b15bed87
“Daniel <danielanderson.lopes0@gmail.com>
2024-08-05 11:38:22 -0300

    ✨ feat: Adiciona método para retornar uma lista de motos filtrada pelo status em MotorcycleRepository

--- src/main/java/br/com/danllopes/autostoreweb/repositories/MotorcycleRepository.java
+++ src/main/java/br/com/danllopes/autostoreweb/repositories/MotorcycleRepository.java
@@ -1,9 +1,13 @@
 package br.com.danllopes.autostoreweb.repositories;
 
 import br.com.danllopes.autostoreweb.domain.entities.Motorcycle;
+import br.com.danllopes.autostoreweb.domain.enums.VehicleStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MotorcycleRepository extends JpaRepository<Motorcycle, String> {
+    List<Motorcycle> findByStatus(VehicleStatus status);
 }
