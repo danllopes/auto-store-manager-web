de2cbfbc6cbdc3af57d5c9ded88c57c0bafdaaae
“Daniel <danielanderson.lopes0@gmail.com>
2024-08-05 11:38:02 -0300

    ✨ feat: Adiciona método para retornar uma lista de carros filtrada pelo status em CarRepository

--- src/main/java/br/com/danllopes/autostoreweb/repositories/CarRepository.java
+++ src/main/java/br/com/danllopes/autostoreweb/repositories/CarRepository.java
@@ -1,9 +1,13 @@
 package br.com.danllopes.autostoreweb.repositories;
 
 import br.com.danllopes.autostoreweb.domain.entities.Car;
+import br.com.danllopes.autostoreweb.domain.enums.VehicleStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car, String> {
+    List<Car> findByStatus(VehicleStatus status);
 }
