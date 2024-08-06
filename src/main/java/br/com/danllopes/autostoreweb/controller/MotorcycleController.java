9e861ce30490f24dbd7d65a7402f0d4cae3823a6
“Daniel <danielanderson.lopes0@gmail.com>
2024-08-06 11:11:52 -0300

    ✨ feat: Documenta a classe MotorcycleController com o Swagger

--- src/main/java/br/com/danllopes/autostoreweb/controller/MotorcycleController.java
+++ src/main/java/br/com/danllopes/autostoreweb/controller/MotorcycleController.java
@@ -3,6 +3,7 @@ package br.com.danllopes.autostoreweb.controller;
 import br.com.danllopes.autostoreweb.domain.entities.Motorcycle;
 import br.com.danllopes.autostoreweb.dtos.MotorcycleRequestDTO;
 import br.com.danllopes.autostoreweb.services.MotorcycleService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -22,28 +23,33 @@ public class MotorcycleController {
     }
 
     @PostMapping("/register")
+    @Operation(summary = "Registrar nova moto", description = "Cria uma nova moto com os dados fornecidos.", tags = "Motorcycle")
     public ResponseEntity<Motorcycle> createMotorcycle(@RequestBody @Valid MotorcycleRequestDTO data) {
         return new ResponseEntity<>(this.service.createMotorcycle(data), HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Listar todas as motos", description = "Retorna uma lista de todas as motos cadastradas.", tags = "Motorcycle")
     public ResponseEntity<List<Motorcycle>> getAllMotorcycles() {
         return new ResponseEntity<>(this.service.getAllMotorcycles(), HttpStatus.OK);
     }
 
     @GetMapping("/{motorcycleId}")
+    @Operation(summary = "Buscar moto por ID", description = "Retorna uma moto específica com base no ID fornecido.", tags = "Motorcycle")
     public ResponseEntity<Motorcycle> getOptionalMotorcycle(@PathVariable String motorcycleId) {
         return new ResponseEntity<>(this.service.getOptionalMotorcycle(motorcycleId), HttpStatus.OK);
     }
 
     @PutMapping("/{motorcycleId}")
     @Transactional
+    @Operation(summary = "Atualizar moto", description = "Atualiza os dados de ums moto existente com base no ID fornecido.", tags = "Motorcycle")
     public ResponseEntity<Motorcycle> updateMotorcycle(@RequestBody @Valid MotorcycleRequestDTO data, @PathVariable String motorcycleId) {
         return new ResponseEntity<>(this.service.updateMotorcycle(data, motorcycleId), HttpStatus.OK);
     }
 
     @DeleteMapping("/{motorcycleId}")
     @Transactional
+    @Operation(summary = "Excluir moto", description = "Exclui uma moto com base no ID fornecido.", tags = "Motorcycle")
     public ResponseEntity<Void> updateToSold(@PathVariable String motorcycleId) {
         this.service.updateToSold(motorcycleId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
